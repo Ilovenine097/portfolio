9f853f75ba941d84969f53489b36874fa200d2d1
Robert Jr. Maestro <ilovenine097@yahoo.com>
2022-02-23 18:18:45 +0800

    upload-resume

---
+++ public/js/app.js
@@ -0,0 +1,11 @@
+AOS.init({
+    easing: 'ease-out-back',
+    duration: 1000
+  });
+ 
+ var typed = new Typed('.typed', {
+        strings: ['UI / UX DEVELOPER', 'GRAPHICS DESIGNER', 'FRONT-END DEVELOPER', 'PHOTOGRAPHER'],
+        typeSpeed: 70,
+        backSpeed: 30,
+        loop: true,
+      });
---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+
+module.exports = {
+  content: [
+    './public/*.html'
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif']
+      },
+      colors: {
+        primary: '#1E40AF',
+        nuetral: '#F1F5F9',
+        secondary: '#1e293b',
+        cscgreen: '#8dad16',
+        cscred: '#e91e63',
+        ojtnavyblue: '#2c3e50',
+        ojtcyan: '#18bc9c',
+        darkcyan: '#64ffda',
+        darkbg: '#0a192f',
+        darktext: '#ccd6f6',
+        darkbglight: '#112240',
+        darkwhite: '#152b51',
+        info: '#2750ce'
+      },
+      backgroundImage: {
+        aboutbg: "url('./public/images/bg.jpg')",
+      }
+    }
+  },
+  plugins: [],
+}
