abbc6e9e010fd5b4e699c54d80f20bf8b7c827ed
Robert Jr. Maestro <ilovenine097@yahoo.com>
2022-03-04 20:12:47 +0800

    a

--- public/js/app.js
+++ public/js/app.js
@@ -7,4 +7,35 @@ AOS.init({
         typeSpeed: 70,
         backSpeed: 30,
         loop: true,
-      });
+});
+
+var scrollpos = window.scrollY;
+var header = document.getElementById("header");
+var navcontent = document.getElementById("nav-content");
+
+document.addEventListener("scroll", function () {
+  /*Apply classes for slide in bar*/
+  scrollpos = window.scrollY;
+
+  if (scrollpos > 10) {
+    header.classList.add("bg-primary");
+    header.classList.add("shadow");
+  } else {
+    header.classList.remove("bg-primary");
+    header.classList.remove("shadow");
+    header.classList.remove("shadow");
+  }
+});
+
+const checkbox = document.querySelector("#checkbox");
+const html = document.querySelector("html");
+const toggleDarkMode = function () {
+  checkbox.checked
+  ? html.classList.add("dark")
+  : html.classList.remove("dark");
+}
+ //calling the function directly
+  toggleDarkMode();
+  checkbox.addEventListener("click",toggleDarkMode);
+
+      
