40b678a3d0e3715c926563518b8b21830decf880
Robert Jr. Maestro <ilovenine097@yahoo.com>
2022-03-25 10:22:36 +0800

    a

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,4 @@
 
-let modal_close = document.getElementById("modal-close");
-let modal = document.getElementById("modal");
-let modal_overlay = document.getElementById("modal_overlay");
-let ux_button = document.getElementById("ux-btn");
-ux_button.addEventListener("click", function(){
-  modal.classList.remove("invisible");
-  modal_overlay.classList.remove("invisible");
-})
-modal_close.addEventListener("click", function(){
-  modal.classList.add("invisible");
-  modal_overlay.classList.add("invisible");
-})
 
 AOS.init({
     duration: 2000
@@ -73,4 +61,42 @@ const toggleDarkMode = function () {
       sidebar.classList.toggle('active');
       overlay.classList.toggle('active');
       sidebarBtn.classList.toggle('open')
+  })
+
+  let modal_close = document.getElementById("modal-close");
+  let modal = document.getElementById("modal");
+  let modal_overlay = document.getElementById("modal_overlay");
+  let ux_button = document.getElementById("ux-btn");
+  let graphics_btn = document.getElementById("graphics-btn");
+  let modal1 = document.getElementById("modal1");
+  let modal1_close = document.getElementById("modal1-close");
+  let photography_btn = document.getElementById("photography-btn");
+  let modal2 = document.getElementById("modal2");
+  let modal2_close = document.getElementById("modal2-close");
+
+  ux_button.addEventListener("click", function(){
+    modal.classList.remove("invisible");
+    modal_overlay.classList.remove("invisible");
+  })
+  modal_close.addEventListener("click", function(){
+    modal.classList.add("invisible");
+    modal_overlay.classList.add("invisible");
+  })
+
+  graphics_btn.addEventListener("click", function(){
+    modal1.classList.remove("invisible");
+    modal_overlay.classList.remove("invisible");
+  })
+  modal1_close.addEventListener("click", function(){
+    modal1.classList.add("invisible");
+    modal_overlay.classList.add("invisible");
+  })
+
+  photography_btn.addEventListener("click", function(){
+    modal2.classList.remove("invisible");
+    modal_overlay.classList.remove("invisible");
+  })
+  modal2_close.addEventListener("click", function(){
+    modal2.classList.add("invisible");
+    modal_overlay.classList.add("invisible");
   })
